df87da454f17ceb06ca449daced39c57bc4e93a5
sarath <sarath@askme.in>
2018-07-04 11:50:43 +0530

    cadisplay link animation done

--- CADisplayLink/ViewController.swift
+++ CADisplayLink/ViewController.swift
@@ -10,10 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let countingLabel: UILabel  = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "1234"
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        return label
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(countingLabel)
+        countingLabel.frame = view.frame
+        let displayLink = CADisplayLink(target: self, selector: #selector(handelUpdate))
+        displayLink.add(to: .main, forMode:.defaultRunLoopMode)
+ 
     }
+    
+    var startValue: Double = 500
+    let endValue: Double   = 1000
+    let animationDuration: Double = 3.5
+    let animationStartDate = Date()
+    
+    
+    @objc func handelUpdate() {
+        
+        let now = Date()
+        
+        let elapsedTime = now.timeIntervalSince(animationStartDate)
+        
+        if elapsedTime > animationDuration{
+            self.countingLabel.text = "\(endValue)"
+
+        }else{
+        let percentage = elapsedTime / animationDuration
+        let value = startValue + percentage * (endValue - startValue)
+        self.countingLabel.text = "\(value)"
+        }
+        
+    }
+//    func stopDisplayLink() {
+//        displayLink?.invalidate()
+//        displayLink = nil
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
